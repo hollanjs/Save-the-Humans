0f3df83d0aaa4e904e625c1937ffbf8be7439102
Josh Hollandsworth <jshollandsworth@gmail.com>
2017-03-06 17:48:14 -0500

    big updates to XAML: added title, start button, progress bar, gradient play area and start to alien head template.

--- Save the Humans/MainPage.xaml.cs
+++ Save the Humans/MainPage.xaml.cs
@@ -10,19 +10,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Save_the_Humans
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainPage : Window
     {
-        public MainWindow()
+        public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
